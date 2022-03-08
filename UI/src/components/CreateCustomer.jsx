21fca30718faa569f3901a920e916759fd69d500
УкрТехноСервис <rvy@ukr.net>
2022-03-09 00:32:31 +0200

    save

--- UI/src/components/CreateCustomer.jsx
+++ UI/src/components/CreateCustomer.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import Button from "@material-ui/core/button";
+
 
 const CreateCustomer = () => {
 
-    const submit = async (data) => {
-        const {name, email, age} = data;
-        const url = "/customer";
+    const inputName = useRef(null);
+    const inputEmail = useRef(null);
+    const inputAge = useRef(null);
+
+    const submit = async (event) => {
+        event.preventDefault();
+
+        const name = inputName.current.value;
+        const email = inputEmail.current.value;
+        const age = inputAge.current.value;
+
+        const url = "/customers";
 
         const create = await fetch(url, {
             method: "POST",
@@ -26,11 +36,14 @@ const CreateCustomer = () => {
 
 
     return (
-        <form /*action={submit} method="POST"*/>
-            customer-name: <input name="name" type="text" placeholder="input customer name"/><br/>
-            customer-name: <input name="email" type="text" placeholder="input customer email"/><br/>
-            customer-name: <input name="age" type="number" placeholder="input customer age"/><br/>
-            submit: <Button type="button" color="primary" variant="contained" onClick={submit}>submit</Button>
+        <form onSubmit={submit}>
+            <label>name
+                <input ref={inputName} name="name" type="text" placeholder="input customer name"/></label><br/>
+            <label>email
+                <input ref={inputEmail} name="email" type="text" placeholder="input customer email"/></label><br/>
+            <label>age
+                <input ref={inputAge} name="age" type="number" placeholder="input customer age"/></label><br/>
+            submit: <Button type="submit" color="primary" variant="contained">submit</Button>
         </form>
     );
 };
